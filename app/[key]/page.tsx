4f6e282cbddfa2358b316e6cfef410fd7c39d25e
Ayush Kumar <ayushk2020@gmail.com>
2023-05-16 00:44:07 -0400

    Download page

--- app/[key]/page.tsx
+++ app/[key]/page.tsx
@@ -1,42 +1,35 @@
-'use client';
+'use client'
 import '@root/global.scss';
-import { useState, useEffect } from 'react';
 import DefaultLayout from '@components/DefaultLayout';
+import styles from '@styles/Download.module.scss';
 
-export default function Page({ params }: { params: { key: string } }) {
-  const baseURL = 'https://gateway.estuary.tech/gw/ipfs/';
-  const [cid, setCid] = useState(null);
-  const [fileName, setFileName] = useState(null);
-  const [fileSize, setFileSize] = useState(null);
+export default async function Page({ params }: { params: { key: string } }) {
+  const getFileInfo = async () => {
+    const response = await fetch(` http://localhost:3005/api/${params.key}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
 
-  useEffect(() => {
-    const getCid = async () => {
-      const response = await fetch(`/api/${params.key}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      const data = await response.json();
-      console.log(data);
-      setCid(data.Cid);
-      setFileName(data.FileName);
-      setFileSize(data.FileSize);
-
-      
-     
-
-    };
     
-    getCid();
-  }, []);
-  
-  
-  return <DefaultLayout>
-    <div>File Name: {fileName}</div>
-    <div>File Size: {fileSize} Bytes</div>
-    <div>Your file is available at: <a href={"https://gateway.estuary.tech/gw/ipfs/" + cid}>{"https://gateway.estuary.tech/gw/ipfs/" + cid}</a></div>
-    <div>Native file viewing is coming soon...</div>
 
-  </DefaultLayout>;
+    return { cid: data.Cid, name: data.FileName, size: data.FileSize };
+  };
+
+  const {cid,name,size } = await getFileInfo();
+
+  return (
+    <DefaultLayout>
+      <div className={styles.infoContainer}>
+        <div className={styles.file}>{name}</div>
+        <div>{size} Bytes</div>
+
+        <a className={styles.downloadButton} href={'https://gateway.estuary.tech/gw/ipfs/' + cid}>
+          <div>Download</div>
+        </a>
+      </div>
+    </DefaultLayout>
+  );
 }
